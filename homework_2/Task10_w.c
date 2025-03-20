3f118779f0cc76ed10e426cf9e08888a338d442c
Garnik Marutyan <garnikmarutyan32@gmail.com>
2025-03-21 02:22:15 +0400

    commit 3

--- homework_2/Task10_w.c
+++ homework_2/Task10_w.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main () {
 	int i = 0;
-	while (i<=10) {
+	while (i <= 10) {
 	printf("%d ", i);
 	i++;
 	}
 	printf("\n");
-
 	i = 10;
-	while (i>=0) {
+	while (i >= 0) {
 	printf("%d ",i);
 	i--;
 	}
