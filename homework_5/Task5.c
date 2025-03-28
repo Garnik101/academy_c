52c8345cb7deaa46feb41809f92d82759c09edd6
Garnik Marutyan <garnikmarutyan32@gmail.com>
2025-03-28 11:15:11 +0400

    Adding Homework 5 | 28.03.2025

--- homework_5/Task5.c
+++ homework_5/Task5.c
@@ -3,7 +3,9 @@
 int main () {
 	int a = 5;
 	int* ptr = NULL;
-	if (ptr == 0) printf("ptr pointer is NULL.\n");
+	if (ptr == 0) {
+		printf("ptr pointer is NULL.\n");
+	}
 	ptr = &a;
 	printf("The value of a is %d.\n", *ptr);
 	return 0;
